e54f8685a9faae5255db279c30fac5ce854bda30
Michael Hinrichs <michaelghinrichs@gmail.com>
2018-11-07 13:11:47 -0800

    Results for all the Hydra clients

--- index.js
+++ index.js
@@ -10,76 +10,130 @@ const homedir = require("os").homedir();
 const pathToHydra = `${homedir}/Code/Procore/procore/hydra_clients`;
 const pathToTmp = `${process.cwd()}/.tmp`;
 const pathToSrc = `${pathToTmp}/hydra_clients`;
+const TEN_MEGA_BYTE = 1024 * 1024 * 10;
+const execOptions = {
+  maxBuffer: TEN_MEGA_BYTE
+};
 
 mkdirp(pathToSrc, mkSrcErr => {
   if (mkSrcErr) throw mkSrcErr;
   fs.readdir(pathToHydra, (readHydraErr, filenames) => {
     if (readHydraErr) throw readHydraErr;
     const copyFiles = filenames.map(filename => {
-      const packagePath = `${pathToHydra}/${filename}/package.json`;
+      const oldHydraClientPath = `${pathToHydra}/${filename}`;
+      const oldLockPath = `${oldHydraClientPath}/yarn.lock`;
+      const oldPackagePath = `${oldHydraClientPath}/package.json`;
       const newHydraClientFolder = `${pathToSrc}/${filename}`;
       const newHydraClientPackagePath = `${newHydraClientFolder}/package.json`;
+      const newHydraClientLockPath = `${newHydraClientFolder}/yarn.lock`;
 
       return new Promise(resolve => {
         mkdirp(newHydraClientFolder, mkdirErr => {
           if (mkdirErr) throw mkdirErr;
-          fs.copyFile(packagePath, newHydraClientPackagePath, cpErr => {
-            if (cpErr) throw cpErr;
+          fs.copyFile(oldLockPath, newHydraClientLockPath, cpLockErr => {
+            if (cpLockErr) throw cpLockErr;
+            fs.copyFile(oldPackagePath, newHydraClientPackagePath, cpErr => {
+              if (cpErr) throw cpErr;
 
-            let newHydraClientPackageFile = require(newHydraClientPackagePath);
-            newHydraClientPackageFile.devDependencies = {};
-            fs.writeFile(
-              newHydraClientPackagePath,
-              JSON.stringify(newHydraClientPackageFile, null, 2),
-              err => {
-                if (err) throw err;
+              let newHydraClientPackageFile = require(newHydraClientPackagePath);
+              newHydraClientPackageFile.devDependencies = {};
+              fs.writeFile(
+                newHydraClientPackagePath,
+                JSON.stringify(newHydraClientPackageFile, null, 2),
+                err => {
+                  if (err) throw err;
 
-                resolve(newHydraClientPackagePath);
-              }
-            );
+                  resolve(newHydraClientPackagePath);
+                }
+              );
+            });
           });
         });
       });
     });
 
     Promise.all([...copyFiles]).then(allFiles => {
-      const gatherAudit = allFiles.slice(0, 4).map(file => {
-        return new Promise((resolve, reject) => {
-          console.log("installing:", file);
-          const directoryPath = path.dirname(file);
-          const directoryName = path.basename(directoryPath);
+      const gatherChecks = allFiles.map(file => {
+        const directoryPath = path.dirname(file);
+        const runOutdated = new Promise((resolve, reject) => {
+          console.log("checking outdated packages...", file);
           process.chdir(directoryPath);
-          exec("yarn audit --json", (yarnErr, stdout, stderr) => {
-            console.log(yarnErr);
-            console.log(stderr);
-            const formatted = stdout
-              .trim()
-              .split(/\r?\n/)
-              .map(f => JSON.parse(f));
-            const final = [{ type: "name", data: directoryName }, ...formatted];
-            resolve(final);
+          exec(
+            "yarn outdated --json",
+            execOptions,
+            (yarnErr, stdout, stderr) => {
+              console.log(yarnErr);
+              console.log(stderr);
+              let formatted = [];
+              try {
+                formatted = stdout
+                  .trim()
+                  .split(/\r?\n/)
+                  .map(f => JSON.parse(f));
+              } catch (e) {
+                throw e;
+              }
+
+              resolve({ outdated: formatted });
+            }
+          );
+        });
+
+        const checkTests = new Promise(resolve => {
+          fs.readFile(file, "utf8", function(err, data) {
+            if (err) throw err;
+            var obj = JSON.parse(data);
+
+            var hasTests = obj.scripts.test !== "true";
+            resolve({ tests: hasTests });
           });
         });
+
+        const runAudit = new Promise((resolve, reject) => {
+          console.log("auditing dependencies...", file);
+          process.chdir(directoryPath);
+          exec("yarn audit --json", execOptions, (yarnErr, stdout, stderr) => {
+            console.log(yarnErr);
+            console.log(stderr);
+            let formatted = [];
+            try {
+              formatted = stdout
+                .trim()
+                .split(/\r?\n/)
+                .map(f => JSON.parse(f));
+            } catch (e) {
+              console.log(e);
+            }
+
+            resolve({ audit: formatted });
+          });
+        });
+
+        return Promise.all([runAudit, runOutdated, checkTests]).then(
+          allChecks => {
+            const directoryName = path.basename(directoryPath);
+
+            const flattened = allChecks.reduce((a, c) => ({ ...a, ...c }));
+
+            return {
+              name: directoryName,
+              checks: flattened
+            };
+          }
+        );
       });
 
-      Promise.all(gatherAudit)
-        .then(auditResults => {
+      Promise.all(gatherChecks)
+        .then(finalResults => {
           const resultFile = `${pathToTmp}/results.json`;
           fs.writeFile(
             resultFile,
-            JSON.stringify(auditResults, null, 2),
+            JSON.stringify(finalResults, null, 2),
             auditResultErr => {
               if (auditResultErr) throw auditResultErr;
               console.log(`Output saved to ${resultFile}!`);
             }
           );
-          // console.log(auditResults);
-          // auditResults.map(result => {
-          //   // console.log("result", result);
-          // });
-          // console.log(af);
-
-          // af[0].one.map(s => console.log("hi", JSON.parse(s)));
         })
         .catch(auditError => {
           throw auditError;
@@ -96,23 +150,6 @@ mkdirp(pathToSrc, mkSrcErr => {
     //       resolve({
     //         hydraClient: filename,
     //         hasBugsnag: false,
-    //       });
-    //     });
-    //   });
-    // });
-
-    // const gatherTests = filenames.map((filename) => {
-    //   var path = `${pathToHydra}/${filename}/package.json`
-
-    //   return new Promise((resolve) => {
-    //     fs.readFile(path, 'utf8', function (err, data) {
-    //       if (err) throw err;
-    //       var obj = JSON.parse(data);
-
-    //       var hasTests = obj.scripts.test !== 'true';
-    //       resolve({
-    //         hydraClient: filename,
-    //         hasTests,
     //       });
     //     });
     //   });
