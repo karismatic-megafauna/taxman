23ccf38cd38ecc11ea2dc616948d4c172a0bc547
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-11-13 11:30:56 -0800

    added advisories to audit data - prevent copying invalid directories

--- index.js
+++ index.js
@@ -1,164 +1,181 @@
-const fs = require("fs");
-const { exec } = require("child_process");
-const path = require("path");
-const npm = require("npm");
-const mkdirp = require("mkdirp");
-const lodash = require("lodash");
-const cliArg = process.argv.slice(2);
-const homedir = require("os").homedir();
+const fs = require('fs')
+const { exec } = require('child_process')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const lodash = require('lodash')
+const cliArg = process.argv.slice(2)
+const homedir = require('os').homedir()
 
-const pathToHydra = `${homedir}/Code/Procore/procore/hydra_clients`;
-const pathToTmp = `${process.cwd()}/.tmp`;
-const pathToSrc = `${pathToTmp}/hydra_clients`;
-const TEN_MEGA_BYTE = 1024 * 1024 * 10;
+const pathToHydra = cliArg[0]
+const pathToTmp = path.resolve(process.cwd(), '.tmp')
+const pathToSrc = path.resolve(pathToTmp, 'hydra_clients')
+const TEN_MEGA_BYTE = 1024 * 1024 * 10
 const execOptions = {
-  maxBuffer: TEN_MEGA_BYTE
-};
-
-const onlyUnique = (value, index, self) => {
-  return self.indexOf(value) === index;
-};
+  maxBuffer: TEN_MEGA_BYTE,
+}
 
 mkdirp(pathToSrc, mkSrcErr => {
-  if (mkSrcErr) throw mkSrcErr;
+  if (mkSrcErr) throw mkSrcErr
   fs.readdir(pathToHydra, (readHydraErr, filenames) => {
-    if (readHydraErr) throw readHydraErr;
-    const copyFiles = filenames.map(filename => {
-      const oldHydraClientPath = `${pathToHydra}/${filename}`;
-      const oldLockPath = `${oldHydraClientPath}/yarn.lock`;
-      const oldPackagePath = `${oldHydraClientPath}/package.json`;
-      const newHydraClientFolder = `${pathToSrc}/${filename}`;
-      const newHydraClientPackagePath = `${newHydraClientFolder}/package.json`;
-      const newHydraClientLockPath = `${newHydraClientFolder}/yarn.lock`;
+    if (readHydraErr) throw readHydraErr
+    const foldersOnly = filenames.filter(filename => {
+      const folderPath = path.resolve(pathToHydra, filename)
+      const stats = fs.statSync(folderPath)
+      if (!stats.isDirectory()) return false
+      return fs.existsSync(path.resolve(folderPath, 'package.json'))
+    })
+    const copyFiles = foldersOnly.map(filename => {
+      const oldHydraClientPath = path.resolve(pathToHydra, filename)
+      const oldLockPath = path.resolve(oldHydraClientPath, 'yarn.lock')
+      const oldPackagePath = path.resolve(oldHydraClientPath, 'package.json')
+      const newHydraClientFolder = path.resolve(pathToSrc, filename)
+      const newHydraClientPackagePath = path.resolve(
+        newHydraClientFolder,
+        'package.json'
+      )
+      const newHydraClientLockPath = path.resolve(
+        newHydraClientFolder,
+        'yarn.lock'
+      )
 
       return new Promise(resolve => {
         mkdirp(newHydraClientFolder, mkdirErr => {
-          if (mkdirErr) throw mkdirErr;
+          if (mkdirErr) throw mkdirErr
           fs.copyFile(oldLockPath, newHydraClientLockPath, cpLockErr => {
-            if (cpLockErr) throw cpLockErr;
+            if (cpLockErr) throw cpLockErr
             fs.copyFile(oldPackagePath, newHydraClientPackagePath, cpErr => {
-              if (cpErr) throw cpErr;
+              if (cpErr) throw cpErr
 
-              let newHydraClientPackageFile = require(newHydraClientPackagePath);
-              newHydraClientPackageFile.devDependencies = {};
+              let newHydraClientPackageFile = require(newHydraClientPackagePath)
+              newHydraClientPackageFile.devDependencies = {}
               fs.writeFile(
                 newHydraClientPackagePath,
                 JSON.stringify(newHydraClientPackageFile, null, 2),
                 err => {
-                  if (err) throw err;
+                  if (err) throw err
 
-                  resolve(newHydraClientPackagePath);
+                  resolve(newHydraClientPackagePath)
                 }
-              );
-            });
-          });
-        });
-      });
-    });
+              )
+            })
+          })
+        })
+      })
+    })
 
     Promise.all([...copyFiles]).then(allFiles => {
       const gatherChecks = allFiles.map(file => {
-        const directoryPath = path.dirname(file);
+        const directoryPath = path.dirname(file)
 
         const checkTests = new Promise(resolve => {
-          fs.readFile(file, "utf8", function(err, data) {
-            if (err) throw err;
-            var obj = JSON.parse(data);
+          fs.readFile(file, 'utf8', function(err, data) {
+            if (err) throw err
+            var obj = JSON.parse(data)
 
-            var hasTests = obj.scripts.test !== "true";
-            resolve({ tests: hasTests });
-          });
-        });
+            var hasTests = obj.scripts.test !== 'true'
+            resolve({ tests: hasTests })
+          })
+        })
 
         const runOutdated = new Promise((resolve, reject) => {
-          console.log("checking outdated packages...", file);
-          process.chdir(directoryPath);
+          console.log('checking outdated packages...', file)
+          process.chdir(directoryPath)
           exec(
-            "yarn outdated --json",
+            'yarn outdated --json',
             execOptions,
             (yarnErr, stdout, stderr) => {
-              console.log(yarnErr);
-              console.log(stderr);
-              let formatted = [];
+              console.log(yarnErr)
+              console.log(stderr)
+              let formatted = []
               try {
                 formatted = stdout
                   .trim()
                   .split(/\r?\n/)
-                  .map(f => JSON.parse(f));
+                  .map(f => JSON.parse(f))
               } catch (e) {
-                throw e;
+                throw e
               }
 
-              resolve({ outdated: formatted });
+              resolve({ outdated: formatted })
             }
-          );
-        });
+          )
+        })
 
         const runAudit = new Promise((resolve, reject) => {
-          console.log("auditing dependencies...", file);
-          process.chdir(directoryPath);
-          exec("yarn audit --json", execOptions, (yarnErr, stdout, stderr) => {
-            console.log(yarnErr);
-            console.log(stderr);
-            let formatted = [];
+          console.log('auditing dependencies...', file)
+          process.chdir(directoryPath)
+          exec('yarn audit --json', execOptions, (yarnErr, stdout, stderr) => {
+            console.log(yarnErr)
+            console.log(stderr)
+            let formatted = []
             try {
               formatted = stdout
                 .trim()
                 .split(/\r?\n/)
-                .map(f => JSON.parse(f));
+                .map(f => JSON.parse(f))
             } catch (e) {
-              console.log(e);
+              console.log(e)
             }
 
             const advisoriesOnly = formatted.filter(
-              f => f.type === "auditAdvisory"
-            );
+              f => f.type === 'auditAdvisory'
+            )
+            const set = new Set()
             const uniqueAdvisories = advisoriesOnly
               .map(ao => {
-                return ao.data.advisory.module_name;
+                return {
+                  module_name: ao.data.advisory.module_name,
+                  advisory: ao.data.advisory,
+                }
               })
-              .filter(onlyUnique);
+              .filter(value => {
+                if (set.has(value.module_name)) {
+                  return false
+                }
+                set.add(value.module_name)
+                return true
+              })
             const summariesOnly = formatted.filter(
-              f => f.type === "auditSummary"
-            );
+              f => f.type === 'auditSummary'
+            )
             const final = [
               ...summariesOnly,
-              { type: "uniqueAdvisories", data: uniqueAdvisories }
-            ];
+              { type: 'uniqueAdvisories', data: uniqueAdvisories },
+            ]
 
-            resolve({ audit: final });
-          });
-        });
+            resolve({ audit: final })
+          })
+        })
 
         return Promise.all([runAudit, runOutdated, checkTests]).then(
           allChecks => {
-            const directoryName = path.basename(directoryPath);
+            const directoryName = path.basename(directoryPath)
 
-            const flattened = allChecks.reduce((a, c) => ({ ...a, ...c }));
+            const flattened = allChecks.reduce((a, c) => ({ ...a, ...c }))
 
             return {
               hydra_client: directoryName,
-              checks: flattened
-            };
+              checks: flattened,
+            }
           }
-        );
-      });
+        )
+      })
 
       Promise.all(gatherChecks)
         .then(finalResults => {
-          const resultFile = `${pathToTmp}/results.json`;
+          const resultFile = `${pathToTmp}/results.json`
           fs.writeFile(
             resultFile,
             JSON.stringify(finalResults, null, 2),
             auditResultErr => {
-              if (auditResultErr) throw auditResultErr;
-              console.log(`Output saved to ${resultFile}!`);
+              if (auditResultErr) throw auditResultErr
+              console.log(`Output saved to ${resultFile}!`)
             }
-          );
+          )
         })
         .catch(auditError => {
-          throw auditError;
-        });
-    });
-  });
-});
+          throw auditError
+        })
+    })
+  })
+})
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /**
+   * This prevent any global prettier configuration and enforces the default
+   * prettier configuration.
+   *
+   * For example, if you have your own custom prettier config file located at
+   * ~/.prettierrc, this PR will override it and prevent it from being applied
+   * to our tool
+   */
+};
